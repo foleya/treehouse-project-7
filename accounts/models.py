e002c42df5564c8d395a47f1c268611728e58605
Andrew Foley <Dewey@Deweys-iMac.local>
2018-05-21 15:08:13 -0400

    Make profile model
    first pass at making the profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    birthday = models.DateField()
+    bio = models.TextField()
+    avatar = models.ImageField(upload_to="avatars")
