7fec3460bbdc3a6ba80a7c08193a297f41733664
Andrew Foley <Dewey@Deweys-iMac.local>
2018-05-22 12:05:30 -0400

    Extended User Model with Profile Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,25 +1,22 @@
-# from django.contrib.auth.models import User
-# from django.db import models
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-#
-# # Create your models here.
-#
-#
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     first_name = models.CharField(max_length=150)
-#     last_name = models.CharField(max_length=150)
-#     email = models.EmailField(unique=True)
-#     birthday = models.DateField()
-#     bio = models.TextField()
-#     avatar = models.ImageField(upload_to="avatars")
-#
-#     # @receiver(post_save, sender=User)
-#     # def create_user_profile(sender, instance, created, **kwargs):
-#     #     if created:
-#     #         Profile.objects.create(user=instance)
-#     #
-#     # @receiver(post_save, sender=User)
-#     # def save_user_profile(sender, instance, **kwargs):
-#     #     instance.profile.save()
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True, blank=True)
+    birthday = models.DateField(null=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to="avatars", null=True)
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
