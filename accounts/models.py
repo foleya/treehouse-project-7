da328c87f0913e2d48f45b03ecf94d4d892c0058
Andrew Foley <Dewey@Deweys-iMac.local>
2018-05-22 11:39:09 -0400

    fixed some django 1.9 vs 2.0 issues

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,25 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Profile(models.Model):
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.EmailField(unique=True)
-    birthday = models.DateField()
-    bio = models.TextField()
-    avatar = models.ImageField(upload_to="avatars")
+# from django.contrib.auth.models import User
+# from django.db import models
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+#
+# # Create your models here.
+#
+#
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     first_name = models.CharField(max_length=150)
+#     last_name = models.CharField(max_length=150)
+#     email = models.EmailField(unique=True)
+#     birthday = models.DateField()
+#     bio = models.TextField()
+#     avatar = models.ImageField(upload_to="avatars")
+#
+#     # @receiver(post_save, sender=User)
+#     # def create_user_profile(sender, instance, created, **kwargs):
+#     #     if created:
+#     #         Profile.objects.create(user=instance)
+#     #
+#     # @receiver(post_save, sender=User)
+#     # def save_user_profile(sender, instance, **kwargs):
+#     #     instance.profile.save()
